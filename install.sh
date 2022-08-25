e056c2876b2ba0a87b9f0dc572b8cdd34d5fac75
ital2000 <scaranoalessandro2005@gmail.com>
2022-08-25 13:24:25 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#copy the backup files in the backufiles folder
+echo "copyng backup files..."
+cp -rf ~/.local/share/nvim backupfiles/user/.local/share
+cp -rf ~/.local/share/rofi backupfiles/user/.local/share
+cp -rf ~/.local/share/applications backupfiles/user/.local/share
+
+cp -rf ~/.config/dunst backupfiles/user/.config
+cp -rf ~/.config/feh backupfiles/user/.config
+cp -rf ~/.config/i3 backupfiles/user/.config
+cp -rf ~/.config/i3blocks backupfiles/user/.config
+cp -rf ~/.config/mpv backupfiles/user/.config
+cp -rf ~/.config/nvim backupfiles/user/.config
+cp -rf ~/.config/picom backupfiles/user/.config
+
+cp -rf ~/.gtkrc-2.0 backupfiles/user
+
+cp -rf /etc/systemd/logind.conf backupfiles/system/etc/systemd
+
+#installation phase
+echo "copyng rice files..."
+sudo cp -rf system-files/* /
+cp -rf user-files/* ~/
+echo "installation complete now reboot or logout"
