c409ba0129c4fa16f5b6027f831e487c513e6439
ital2000 <scaranoalessandro2005@gmail.com>
2022-08-28 17:27:09 +0200

    Better installation scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+#file variables
+dunst=~/.config/dunst
+feh=~/.config/feh
+i3=~/.config/i3
+i3blocks=~/.config/i3blocks
+mpv=~/.config/mpv
+nvim=~/.config/nvim
+picom=~/.config/picom
+gtk3=~/.config/gtk-3.0
+gtk4=~/.config/gtk-4.0
+gtk2=~/.gtkrc-2.0
+
 #creating backupfiles directory
 mkdir -p backupfiles/user/.local/share
 mkdir -p backupfiles/user/.config
@@ -7,21 +19,70 @@ mkdir -p backupfiles/system/etc/systemd
 
 #copy the backup files in the backufiles folder
 echo "copyng backup files..."
-cp -rf ~/.local/share/nvim backupfiles/user/.local/share
-cp -rf ~/.local/share/rofi backupfiles/user/.local/share
-cp -rf ~/.local/share/applications backupfiles/user/.local/share
 
-cp -rf ~/.config/dunst backupfiles/user/.config
-cp -rf ~/.config/feh backupfiles/user/.config
-cp -rf ~/.config/i3 backupfiles/user/.config
-cp -rf ~/.config/i3blocks backupfiles/user/.config
-cp -rf ~/.config/mpv backupfiles/user/.config
-cp -rf ~/.config/nvim backupfiles/user/.config
-cp -rf ~/.config/picom backupfiles/user/.config
+if [-d "$dunst"]; then
+    cp -rf ~/.config/dunst backupfiles/user/.config
+fi
 
-cp -rf ~/.gtkrc-2.0 backupfiles/user
+if [-d "$feh"]; then
+    cp -rf ~/.config/feh backupfiles/user/.config
+fi
+
+if [-d "$i3"]; then
+    cp -rf ~/.config/i3 backupfiles/user/.config
+fi
+
+if [-d "$i3blocks"]; then
+    cp -rf ~/.config/i3blocks backupfiles/user/.config
+fi
+
+if [-d "$mpv"]; then
+    cp -rf ~/.config/mpv backupfiles/user/.config
+fi
+
+if [-d "$nvim"]; then
+    cp -rf ~/.config/nvim backupfiles/user/.config
+fi
+
+if [-d "$picom"]; then
+    cp -rf ~/.config/picom backupfiles/user/.config
+fi
+
+if [-d "$gtk3"]; then
+    cp -rf ~/.config/gtk-3.0 backupfiles/user/.config
+fi
+
+if [-d "$gtk4"]; then
+    cp -rf ~/.config/gtk-4.0 backupfiles/user/.config
+fi
+
+if [-f "$gtk2"]; then
+    cp -rf ~/.gtkrc-2.0 backupfiles/user
+fi
 
 cp -rf /etc/systemd/logind.conf backupfiles/system/etc/systemd
+
+#installing gtk themes
+echo "Cloning system themes repositories..."
+
+git clone https://github.com/vinceliuice/Graphite-gtk-theme.git
+git clone https://github.com/vinceliuice/Graphite-cursors.git
+git clone https://github.com/vinceliuice/Tela-icon-theme.git
+
+echo "installing graphite gtk theme..."
+cd Graphite-gtk-theme
+sudo ./install.sh --tweaks rimless black
+cd ..
+
+echo "installing graphite cursors..."
+cd Graphite-cursors
+sudo ./install.sh
+cd ..
+
+echo "installing tela icon theme..."
+cd Tela-icon-theme
+sudo ./install.sh grey
+cd ..
 
 #installation phase
 echo "copyng rice files..."
--- uninstall.sh
+++ uninstall.sh
@@ -3,30 +3,41 @@
 #WARNING DO NOT EXECUTE THIS FILE IF YOU DON'T HAVE
 #INSTALLED THE DOTFILES
 
-echo "Removing rice files..." 
-sudo rm -rf /usr/share/themes/Graphite-Dark-compact
-sudo rm -rf /usr/share/themes/Graphite-Dark-compact-hdpi
-sudo rm -rf /usr/share/themes/Graphite-Dark-compact-xhdpi
-sudo rm -rf /usr/share/icons/Graphite-dark-cursors
-sudo rm -rf /usr/share/icons/Tela-grey-dark
-sudo rm -rf /etc/systemd/logind.conf
+if [-d "backupfiles"]; then
 
-rm -rf ~/.gtkrc-2.0
-rm -rf ~/.local/share/applications
-rm -rf ~/.local/share/backgrounds/wave-dark.png
-rm -rf ~/.local/share/backgrounds/wave-dark-arch.png
-rm -rf ~/.local/share/nvim
-rm -rf ~/.local/share/rofi
+    echo "Removing themes and system files..."
+    sudo rm -rf /usr/share/themes/Graphite-Dark-compact
+    sudo rm -rf /usr/share/themes/Graphite-Dark-compact-hdpi
+    sudo rm -rf /usr/share/themes/Graphite-Dark-compact-xhdpi
+    sudo rm -rf /usr/share/icons/Graphite-dark-cursors
+    sudo rm -rf /usr/share/icons/Tela-grey-dark
+    sudo rm -rf /etc/systemd/logind.conf
 
-rm -rf ~/.config/dunst
-rm -rf ~/.config/feh
-rm -rf ~/.config/i3
-rm -rf ~/.config/i3blocks
-rm -rf ~/.config/mpv
-rm -rf ~/.config/nvim
-rm -rf ~/.config/picom
+    echo "Removing configuration files..."
+    rm -rf ~/.local/share/applications/feh.desktop
+    rm -rf ~/.local/share/applications/picom.desktop
+    rm -rf ~/.local/share/applications/rofi.desktop
+    rm -rf ~/.local/share/applications/rofi-theme-selector.desktop
+    rm -rf ~/.local/share/backgrounds/wave-dark.png
+    rm -rf ~/.local/share/backgrounds/wave-dark-arch.png
+    rm -rf ~/.local/share/nvim
+    rm -rf ~/.local/share/rofi
 
-echo "Copyng backup files..."
-sudo cp -rf backupfiles/system/* /
-cp -rf backupfiles/user/* ~/
-echo "uninstallation complete now reboot"
+    rm -rf ~/.config/dunst
+    rm -rf ~/.config/feh
+    rm -rf ~/.config/i3
+    rm -rf ~/.config/i3blocks
+    rm -rf ~/.config/mpv
+    rm -rf ~/.config/nvim
+    rm -rf ~/.config/picom
+    rm -rf ~/.config/gtk-3.0
+    rm -rf ~/.config/gtk-4.0
+    rm -rf ~/.gtkrc-2.0
+
+    echo "Copyng backup files..."
+    sudo cp -rf backupfiles/system/* /
+    cp -rf backupfiles/user/. ~/
+    echo "uninstallation complete now reboot"
+else
+    echo "The dotfiles are not installed"
+fi
